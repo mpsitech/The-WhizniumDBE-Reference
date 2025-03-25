8b81251ffadbfeea7d0f8d130100672f2f7281f7
Alexander Wirthmueller <aw@mpsitech.com>
2025-03-25 10:45:47 +0100

    rename from doc to ref

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'WhizniumDBE Documentation'
+project = 'WhizniumDBE Reference'
 copyright = '2025, MPSI Technologies GmbH'
 author = 'Alexander Wirthmueller'
 
